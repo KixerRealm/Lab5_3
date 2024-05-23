78090d2d0a108975d893c6baa4a770f08e62a041
Kristijan Kostovski <kristijan.kostovski@teaming.inteligenta.io>
2024-05-23 16:20:28 +0200

    Add project files.

---
+++ MVCAdminApplication/Controllers/OrderController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAdminApplication.Models;
+using Newtonsoft.Json;
+using System.Text;
+using ClosedXML.Excel;
+using GemBox.Spreadsheet;
+using GemBox.Document;
+using PdfSaveOptions = GemBox.Document.PdfSaveOptions;
+
+namespace MVCAdminApplication.Controllers
+{
+    public class OrderController : Controller
+
+    {
+        public OrderController()
+        {
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            string URL = "http://localhost:5054/api/Admin/GetAllOrders";
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+
+            var data = response.Content.ReadAsAsync<List<Order>>().Result;
+            return View(data);
+        }
+
+        public IActionResult Details(Guid Id)
+        {   
+            HttpClient client = new HttpClient();
+            string URL = "http://localhost:5054/api/Admin/GetDetailsForOrder";
+            var model = new
+            {
+                Id = Id
+            };
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var data = response.Content.ReadAsAsync<Order>().Result;
+            return View(data);
+        }
+
+        public FileContentResult CreateInvoice(string id)
+        {
+            HttpClient client = new HttpClient();
+
+            string URL = "http://localhost:5054/api/Admin/GetDetailsForOrder";
+            var model = new
+            {
+                Id = id
+            };
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var result = response.Content.ReadAsAsync<Order>().Result;
+
+            var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Invoice.odt");
+            var document = DocumentModel.Load(templatePath);
+
+            document.Content.Replace("{{OrderNumber}}", result.Id.ToString());
+            document.Content.Replace("{{UserName}}", result.Owner.FirstName);
+
+            StringBuilder sb = new StringBuilder();
+            var total = 0.0;
+            foreach (var item in result.ProductInOrders)
+            {
+                sb.AppendLine("Product " + item.OrderedProduct.Movie + " has quantity " + item.Quantity + " with price " + item.OrderedProduct.Price);
+                total += (item.Quantity * item.OrderedProduct.Price);
+            }
+            document.Content.Replace("{{ProductList}}", sb.ToString());
+            document.Content.Replace("{{TotalPrice}}", total.ToString() + "$");
+
+            var stream = new MemoryStream();
+            document.Save(stream, new PdfSaveOptions());
+            return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
+        }
+
+        [HttpGet]
+        public FileContentResult ExportOrders()
+        {
+            string fileName = "AllOrders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workBook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workBook.Worksheets.Add("Orders");
+
+                worksheet.Cell(1, 1).Value = "Order ID";
+                worksheet.Cell(1, 2).Value = "Customer Username";
+
+                HttpClient client = new HttpClient();
+                string URL = "http://localhost:5054/api/Admin/GetAllOrders";
+                HttpResponseMessage response = client.GetAsync(URL).Result;
+
+                var data = response.Content.ReadAsAsync<List<Order>>().Result;
+
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    var order = data[i];
+                    worksheet.Cell(i + 2, 1).Value = order.Id.ToString();
+                    worksheet.Cell(i + 2, 2).Value = order.Owner.FirstName;
+
+                    for (int j = 0; j < order.ProductInOrders.Count(); j++)
+                    {
+                        worksheet.Cell(1, j + 3).Value = "Product - " + (j + 1);
+                        worksheet.Cell(i + 2, j + 3).Value = order.ProductInOrders.ElementAt(j).OrderedProduct.Movie.MovieName;
+
+                    }
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName);
+                }
+            }
+        }
+    }
+}
